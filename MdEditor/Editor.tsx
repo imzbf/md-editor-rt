60b73df99a2874935ff6c38a04c1a480a38c20d2
之间 <zbfcqtl@163.com>
2021-08-06 17:16:23 +0800

    feat: 编辑器类型申明

--- MdEditor/Editor.tsx
+++ MdEditor/Editor.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cn from 'classnames';
+import { createPortal } from 'react-dom';
+
+import { allToolbar, highlightUrl, iconfontUrl, prettierUrl, cropperUrl } from './config';
 
 declare global {
   interface Window {
@@ -10,34 +13,166 @@ declare global {
   }
 }
 
-export interface Props {
+export interface ToolbarTips {
+  bold?: string;
+  underline?: string;
+  italic?: string;
+  strikeThrough?: string;
+  title?: string;
+  sub?: string;
+  sup?: string;
+  quote?: string;
+  unorderedList?: string;
+  orderedList?: string;
+  codeRow?: string;
+  code?: string;
+  link?: string;
+  image?: string;
+  table?: string;
+  revoke?: string;
+  next?: string;
+  save?: string;
+  prettier?: string;
+  pageFullscreen?: string;
+  fullscreen?: string;
+  preview?: string;
+  htmlPreview?: string;
+  github?: string;
+}
+
+export interface StaticTextDefaultValue {
+  toolbarTips?: ToolbarTips;
+  titleItem?: {
+    h1?: string;
+    h2?: string;
+    h3?: string;
+    h4?: string;
+    h5?: string;
+    h6?: string;
+  };
+  linkModalTips?: {
+    title?: string;
+    descLable?: string;
+    descLablePlaceHolder?: string;
+    urlLable?: string;
+    UrlLablePlaceHolder?: string;
+    buttonOK?: string;
+    buttonUpload?: string;
+    buttonUploadClip?: string;
+  };
+  clipModalTips?: {
+    title?: string;
+    buttonUpload?: string;
+  };
+  copyCode?: {
+    text?: string;
+    tips?: string;
+  };
+}
+
+export interface StaticTextDefault {
+  'zh-CN': StaticTextDefaultValue;
+  'en-US': StaticTextDefaultValue;
+}
+
+export type StaticTextDefaultKey = keyof StaticTextDefault;
+
+export type ToolbarNames = keyof ToolbarTips;
+
+export interface EditorProp {
   modelValue: string;
-  // 主题，支持light和dark
+  // 主题，默认light
   theme?: 'light' | 'dark';
   // 外层扩展类名
   editorClass?: string;
+  // 如果项目中有使用highlight.js或者没有外网访问权限，可以直接传递实例hljs并且手动导入css
+  hljs?: Record<string, unknown>;
+  // 可以手动提供highlight.js的cdn链接
+  highlightJs?: string;
+  highlightCss?: string;
+  // 历史记录最长条数，默认10
+  historyLength?: number;
+  // input回调事件
+  onChange?: (v: string) => void;
+  // 保存事件
+  onSave?: (v: string) => void;
+  // 上传图片事件
+  onUploadImg?: (files: FileList, callBack: (urls: string[]) => void) => void;
+  // 是否页面内全屏，默认false
+  pageFullScreen?: boolean;
+  // 是否展开预览，默认true
+  preview?: boolean;
+  // 是否展开html预览，默认false
+  htmlPreview?: boolean;
+  // 仅预览模式，不显示toolbar和编辑框，默认false
+  previewOnly?: boolean;
+  // 预设语言名称
+  language?: StaticTextDefaultKey | string;
+  // 语言扩展，以标准的形式定义内容，设置language为key值即可替换
+  languageUserDefined?: Array<{ [key: string]: StaticTextDefaultValue }>;
+  // 工具栏选择显示
+  toolbars?: Array<ToolbarNames>;
+  // 工具栏选择不显示
+  toolbarsExclude?: Array<ToolbarNames> | [];
+  // 格式化md，默认true
+  prettier?: boolean;
+  prettierCDN?: string;
+  prettierMDCDN?: string;
+  // html变化事件
+  onHtmlChanged?: (h: string) => void;
+  // 图片裁剪
+  cropperCss?: string;
+  cropperJs?: string;
+  // 图标
+  iconfontJs?: string;
 }
 
 export const prefix = 'md';
 
-const Editor = (props: Props) => {
+// 生成唯一ID
+const editorId = `mev-${Math.random().toString(36).substr(3)}`;
+
+const Editor = (props: EditorProp) => {
+  const [test, setTest] = useState(0);
+
+  setTimeout(() => {
+    setTest((v) => v + 1);
+  }, 1000);
+
   return (
     <div
+      id={editorId}
       className={cn([
         prefix,
         props.editorClass,
         props.theme === 'dark' && `${prefix}-dark`
       ])}
     >
-      编辑器
+      编辑器{test}
+      {createPortal(<script src={props.iconfontJs} />, document.head)}
     </div>
   );
 };
 
 Editor.defaultProps = {
-  modelValue: '',
   theme: 'light',
-  editorClass: ''
-} as Props;
+  editorClass: '',
+  highlightJs: highlightUrl.js,
+  highlightCss: highlightUrl.css,
+  historyLength: 10,
+  pageFullScreen: false,
+  preview: true,
+  htmlPreview: false,
+  previewOnly: false,
+  language: 'zh-CN',
+  toolbars: allToolbar,
+  toolbarsExclude: [],
+  prettier: true,
+  prettierCDN: prettierUrl.main,
+  prettierMDCDN: prettierUrl.markdown,
+  cropperCss: cropperUrl.css,
+  cropperJs: cropperUrl.js,
+  iconfontJs: iconfontUrl
+} as EditorProp;
 
 export default Editor;
