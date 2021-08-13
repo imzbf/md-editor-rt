5ab453da35fa2fc5bc6d078c607cda04903de179
之间 <zbfcqtl@163.com>
2021-08-13 17:29:23 +0800

    fix: 文案语法错误

--- dev/Header/index.tsx
+++ dev/Header/index.tsx
@@ -12,7 +12,7 @@ export default (props: HeaderProp) => (
     <section className="container">
       <h1 className="project-name">md-editor-rt</h1>
       <p className="project-desc">
-        Markdown编辑器，React版本，支持切换主题、Prettier美化文本等，支持SSR渲染，在NextJs中使用。
+        Markdown编辑器，React版本，支持切换主题、Prettier美化文本等，支持SSR，在NextJs中使用。
       </p>
       <p className="header-actions">
         <button className="btn btn-header">
