55b6782ac13493ac0ef489f4b7d0ef00925d2a27
zbf <zbfcqtl@163.com>
2021-11-17 16:04:58 +0800

    feat: 关于页

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Editor from 'md-editor-rt';
+import { useSelector } from 'react-redux';
+import { StateType } from '@/store';
+import { version } from '../../../package.json';
+import axios from '@/utils/request';
 
 const About = () => {
-  return <div>123</div>;
+  const state = useSelector<StateType>((state) => state) as StateType;
+
+  const [mdText, setMdText] = useState('');
+
+  const queryMd = () => {
+    axios
+      .get(`/about-${state.lang}.md`)
+      .then(({ data }: any) => {
+        setMdText((data as string).replace(/\$\{EDITOR_VERSION\}/g, version));
+      })
+      .catch((e) => {
+        console.error(e);
+
+        setMdText('文档读取失败！');
+      });
+  };
+
+  useEffect(queryMd, [state.lang]);
+
+  return (
+    <div className="container">
+      <div className="doc">
+        <div className="content" style={{ width: '100%' }}>
+          <Editor
+            theme={state.theme}
+            modelValue={mdText}
+            previewTheme={state.previewTheme}
+            previewOnly
+            showCodeRowNumber
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
