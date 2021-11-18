b61d3625e531751922077b209369e58c05c75443
zbf <zbfcqtl@163.com>
2021-11-18 10:42:35 +0800

    feat: 文档页面

--- src/pages/Doc/index.tsx
+++ src/pages/Doc/index.tsx
@@ -1,14 +1,53 @@
 import React, { useEffect, useState } from 'react';
-import Editor from 'md-editor-rt';
+import Editor, { HeadList } from 'md-editor-rt';
 import axios from 'axios';
+import { version } from '../../../package.json';
+import { useSelector } from 'react-redux';
+import { StateType } from '@/store';
+import { Affix } from 'antd';
+import Catalog from '@/components/Catalog';
 
 export default () => {
   const [mdText, setMdText] = useState('');
+  const [catalogList, setList] = useState<Array<HeadList>>([]);
+  const state = useSelector<StateType>((state) => state) as StateType;
+
+  const queryMd = () => {
+    axios
+      .get(`/doc-${state.lang}.md`)
+      .then(({ data }) => {
+        setMdText((data as string).replace(/\$\{EDITOR_VERSION\}/g, version));
+      })
+      .catch((e) => {
+        console.error(e);
+
+        setMdText('文档读取失败！');
+      });
+  };
+
+  useEffect(queryMd, [state.lang]);
 
   return (
-    <div className="doc">
-      <div className="container">
-        {/* <Editor editorId="md-doc" theme={props.theme} modelValue={mdText} previewOnly /> */}
+    <div className="container">
+      <div className="doc">
+        <div className="content">
+          <Editor
+            theme={state.theme}
+            language={state.lang}
+            modelValue={mdText}
+            previewTheme={state.previewTheme}
+            previewOnly
+            showCodeRowNumber
+            onGetCatalog={(arr) => {
+              setList(arr);
+            }}
+          />
+        </div>
+        <div className="catalog">
+          <Affix offsetTop={16}>
+            <Catalog heads={catalogList} />
+          </Affix>
+        </div>
       </div>
     </div>
   );
