5955a6c9e3ea46402ec263f7687d30553b6f3fbc
zbf <zbfcqtl@gmail.com>
2022-04-11 22:09:26 +0800

    v1.6.8

--- src/pages/Demo/index.tsx
+++ src/pages/Demo/index.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import Editor, { HeadList } from 'md-editor-rt';
+import Editor from 'md-editor-rt';
 import axios from '@/utils/request';
 import { version } from '../../../package.json';
 import { useSelector } from 'react-redux';
 import { StateType } from '@/store';
 import { Affix } from 'antd';
-import Catalog from '@/components/Catalog';
+
+const Catalog = Editor.Catalog;
 
 export default () => {
   const [mdText, setMdText] = useState('');
-  const [catalogList, setList] = useState<Array<HeadList>>([]);
   const state = useSelector<StateType>((state) => state) as StateType;
 
   const queryMd = () => {
@@ -32,20 +32,22 @@ export default () => {
       <div className="doc">
         <div className="content">
           <Editor
+            editorId="demo-preview"
             theme={state.theme}
             language={state.lang}
             modelValue={mdText}
             previewTheme={state.previewTheme}
             previewOnly
             showCodeRowNumber
-            onGetCatalog={(arr) => {
-              setList(arr);
-            }}
           />
         </div>
         <div className="catalog">
           <Affix offsetTop={16}>
-            <Catalog heads={catalogList} />
+            <Catalog
+              editorId="demo-preview"
+              theme={state.theme}
+              scrollElement={document.documentElement}
+            />
           </Affix>
         </div>
       </div>
