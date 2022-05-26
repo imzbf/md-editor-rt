a196b5b3782a8eec5aa4b5cb491a330e508002df
zbf <zbfcqtl@gmail.com>
2022-05-26 14:45:24 +0800

    chore: next example

---
+++ example/next/src/pages/_app.jsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+import 'md-editor-rt/lib/style.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
