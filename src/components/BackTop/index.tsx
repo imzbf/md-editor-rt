9ab19b3e1369f42f416cd99cc3e72796b0d083ff
zbf <zbfcqtl@gmail.com>
2022-05-31 14:56:59 +0800

    feat: backTop组件

---
+++ src/components/BackTop/index.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+
+const backTop = () => {
+  document.documentElement.scroll({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
+
+const BackTop = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => {
+      setScrollTop(document.documentElement.scrollTop);
+    });
+  }, []);
+
+  return scrollTop > 20 ? (
+    <div className="back-top" onClick={backTop}>
+      <svg className="icon" aria-hidden="true">
+        <use xlinkHref="#icon-top"></use>
+      </svg>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default BackTop;
