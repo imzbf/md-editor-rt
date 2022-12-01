0097ef6a45b855467ee46da4ef425a102e27ab43
zbf <zbfcqtl@gmail.com>
2022-12-01 21:10:40 +0800

    feat: remove configuration of highlight

--- src/main.tsx
+++ src/main.tsx
@@ -28,17 +28,6 @@ MdEditor.config({
     languageUserDefined: {
       ZH_TW
     }
-  },
-  editorExtensions: {
-    highlight: {
-      css: {
-        atom: {
-          light:
-            'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/atom-one-dark.min.css',
-          dark: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/atom-one-dark.min.css'
-        }
-      }
-    }
   }
 });
 
