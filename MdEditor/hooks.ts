ccafa44b7624f886b1fb6ba6b223ec88f7cb96d9
zbf <zbfcqtl@gmail.com>
2022-12-18 20:52:32 +0800

    fix: shortcut key cannot be triggered correctly(#58)

--- MdEditor/hooks.ts
+++ MdEditor/hooks.ts
@@ -52,6 +52,11 @@ import {
  * @param staticProps
  */
 export const useKeyBoard = (props: EditorProp, staticProps: StaticProp) => {
+  const {
+    toolbars = defaultProps.toolbars,
+    toolbarsExclude = defaultProps.toolbarsExclude,
+    modelValue
+  } = props;
   const { editorId, previewOnly, noPrettier } = staticProps;
 
   const [state, setState] = useState({
@@ -61,210 +66,197 @@ export const useKeyBoard = (props: EditorProp, staticProps: StaticProp) => {
     html: ''
   });
 
-  const initFunc = (name: ToolbarNames) =>
-    props.toolbars?.includes(name) && !props.toolbarsExclude?.includes(name);
+  const initFunc = useCallback(
+    (name: ToolbarNames) => {
+      return toolbars?.includes(name) && !toolbarsExclude?.includes(name);
+    },
+    [toolbars, toolbarsExclude]
+  );
 
-  const keyDownHandler = (event: KeyboardEvent) => {
-    // 只处理是编辑框内的内容
-    if (event.target !== document.querySelector(`#${editorId}-textarea`)) {
-      return;
-    }
+  const keyDownHandler = useCallback(
+    (event: KeyboardEvent) => {
+      // 只处理是编辑框内的内容
+      if (event.target !== document.querySelector(`#${editorId}-textarea`)) {
+        return;
+      }
 
-    // 使用快捷键时，保存选中文本
-    bus.emit(editorId, 'selectTextChange');
+      // 使用快捷键时，保存选中文本
+      bus.emit(editorId, 'selectTextChange');
 
-    // macos中以meta键位配s键位为保存，windows中如此会被系统默认的事件替代
-    if (event.ctrlKey || event.metaKey) {
-      switch (event.code) {
-        case 'KeyS': {
-          if (event.shiftKey) {
-            // 删除线
-            if (initFunc('strikeThrough')) {
-              bus.emit(editorId, 'replace', 'strikeThrough' as ToolDirective);
+      // macos中以meta键位配s键位为保存，windows中如此会被系统默认的事件替代
+      if (event.ctrlKey || event.metaKey) {
+        switch (event.code) {
+          case 'KeyS': {
+            if (event.shiftKey) {
+              // 删除线
+              if (initFunc('strikeThrough')) {
+                bus.emit(editorId, 'replace', 'strikeThrough' as ToolDirective);
+              }
+            } else {
+              // 触发保存事件
+              if (initFunc('save')) {
+                bus.emit(editorId, ON_SAVE, modelValue);
+                event.preventDefault();
+              }
             }
-          } else {
-            // 触发保存事件
-            if (initFunc('save')) {
-              bus.emit(editorId, ON_SAVE, props.modelValue);
+            break;
+          }
+          case 'KeyB': {
+            if (initFunc('bold')) {
+              bus.emit(editorId, 'replace', 'bold' as ToolDirective);
               event.preventDefault();
             }
+            break;
           }
-          break;
-        }
-        case 'KeyB': {
-          if (initFunc('bold')) {
-            bus.emit(editorId, 'replace', 'bold' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'KeyU': {
-          if (event.shiftKey) {
-            // ctrl+shift+u触发无需列表
-            if (initFunc('unorderedList')) {
-              bus.emit(editorId, 'replace', 'unorderedList' as ToolDirective);
-              event.preventDefault();
+          case 'KeyU': {
+            if (event.shiftKey) {
+              // ctrl+shift+u触发无需列表
+              if (initFunc('unorderedList')) {
+                bus.emit(editorId, 'replace', 'unorderedList' as ToolDirective);
+                event.preventDefault();
+              }
+            } else {
+              // ctrl+u触发下划线
+              if (initFunc('underline')) {
+                bus.emit(editorId, 'replace', 'underline' as ToolDirective);
+                event.preventDefault();
+              }
             }
-          } else {
-            // ctrl+u触发下划线
-            if (initFunc('underline')) {
-              bus.emit(editorId, 'replace', 'underline' as ToolDirective);
-              event.preventDefault();
-            }
-          }
 
-          break;
-        }
-        case 'KeyI': {
-          if (event.shiftKey) {
-            // ctrl+shift+l触发图片链接
-            if (initFunc('image')) {
-              bus.emit(editorId, 'openModals', 'image');
+            break;
+          }
+          case 'KeyI': {
+            if (event.shiftKey) {
+              // ctrl+shift+l触发图片链接
+              if (initFunc('image')) {
+                bus.emit(editorId, 'openModals', 'image');
+                event.preventDefault();
+              }
+            } else {
+              if (initFunc('italic')) {
+                bus.emit(editorId, 'replace', 'italic' as ToolDirective);
+                event.preventDefault();
+              }
+            }
+
+            break;
+          }
+          case 'Digit1': {
+            if (initFunc('title')) {
+              bus.emit(editorId, 'replace', 'h1' as ToolDirective);
               event.preventDefault();
             }
-          } else {
-            if (initFunc('italic')) {
-              bus.emit(editorId, 'replace', 'italic' as ToolDirective);
+            break;
+          }
+          case 'Digit2': {
+            if (initFunc('title')) {
+              bus.emit(editorId, 'replace', 'h2' as ToolDirective);
               event.preventDefault();
             }
+            break;
           }
+          case 'Digit3': {
+            if (initFunc('title')) {
+              bus.emit(editorId, 'replace', 'h3' as ToolDirective);
+              event.preventDefault();
+            }
+            break;
+          }
+          case 'Digit4': {
+            if (initFunc('title')) {
+              bus.emit(editorId, 'replace', 'h4' as ToolDirective);
+              event.preventDefault();
+            }
+            break;
+          }
+          case 'Digit5': {
+            if (initFunc('title')) {
+              bus.emit(editorId, 'replace', 'h5' as ToolDirective);
+              event.preventDefault();
+            }
+            break;
+          }
+          case 'Digit6': {
+            if (initFunc('title')) {
+              bus.emit(editorId, 'replace', 'h6' as ToolDirective);
+              event.preventDefault();
+            }
+            break;
+          }
+          case 'ArrowUp': {
+            if (initFunc('sup')) {
+              bus.emit(editorId, 'replace', 'sup' as ToolDirective);
+              event.preventDefault();
+            }
+            break;
+          }
+          case 'ArrowDown': {
+            if (initFunc('sub')) {
+              bus.emit(editorId, 'replace', 'sub' as ToolDirective);
+              event.preventDefault();
+            }
+            break;
+          }
+          case 'KeyQ': {
+            if (event.key === 'a') {
+              (event.target as HTMLTextAreaElement).select();
+              return;
+            }
 
-          break;
-        }
-        case 'Digit1': {
-          if (initFunc('title')) {
-            bus.emit(editorId, 'replace', 'h1' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Digit2': {
-          if (initFunc('title')) {
-            bus.emit(editorId, 'replace', 'h2' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Digit3': {
-          if (initFunc('title')) {
-            bus.emit(editorId, 'replace', 'h3' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Digit4': {
-          if (initFunc('title')) {
-            bus.emit(editorId, 'replace', 'h4' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Digit5': {
-          if (initFunc('title')) {
-            bus.emit(editorId, 'replace', 'h5' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'Digit6': {
-          if (initFunc('title')) {
-            bus.emit(editorId, 'replace', 'h6' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'ArrowUp': {
-          if (initFunc('sup')) {
-            bus.emit(editorId, 'replace', 'sup' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'ArrowDown': {
-          if (initFunc('sub')) {
-            bus.emit(editorId, 'replace', 'sub' as ToolDirective);
-            event.preventDefault();
-          }
-          break;
-        }
-        case 'KeyQ': {
-          if (event.key === 'a') {
-            (event.target as HTMLTextAreaElement).select();
-            return;
-          }
-
-          bus.emit(editorId, 'replace', 'quote' as ToolDirective);
-          event.preventDefault();
-          break;
-        }
-        case 'KeyA': {
-          if (event.key === 'q') {
             bus.emit(editorId, 'replace', 'quote' as ToolDirective);
             event.preventDefault();
             break;
-          } else {
-            return;
           }
-        }
-        case 'KeyO': {
-          if (initFunc('orderedList')) {
-            bus.emit(editorId, 'replace', 'orderedList' as ToolDirective);
-            event.preventDefault();
+          case 'KeyA': {
+            if (event.key === 'q') {
+              bus.emit(editorId, 'replace', 'quote' as ToolDirective);
+              event.preventDefault();
+              break;
+            } else {
+              return;
+            }
           }
-          break;
-        }
-        case 'KeyC': {
-          if (event.shiftKey) {
-            // ctrl+shift+c触发块级代码
-            if (initFunc('code')) {
-              bus.emit(editorId, 'replace', 'code' as ToolDirective);
+          case 'KeyO': {
+            if (initFunc('orderedList')) {
+              bus.emit(editorId, 'replace', 'orderedList' as ToolDirective);
               event.preventDefault();
             }
-          } else if (event.altKey) {
-            // ctrl+alt+c触发行内代码
-            if (initFunc('codeRow')) {
-              bus.emit(editorId, 'replace', 'codeRow' as ToolDirective);
-              event.preventDefault();
-            }
-          } else {
-            // 接管复制快捷键
-            event.preventDefault();
-            bus.emit(editorId, 'replace', 'ctrlC');
             break;
           }
-          break;
-        }
-        case 'KeyL': {
-          // ctrl+l触发普通链接
-          if (initFunc('link')) {
-            bus.emit(editorId, 'openModals', 'link');
-            event.preventDefault();
+          case 'KeyC': {
+            if (event.shiftKey) {
+              // ctrl+shift+c触发块级代码
+              if (initFunc('code')) {
+                bus.emit(editorId, 'replace', 'code' as ToolDirective);
+                event.preventDefault();
+              }
+            } else if (event.altKey) {
+              // ctrl+alt+c触发行内代码
+              if (initFunc('codeRow')) {
+                bus.emit(editorId, 'replace', 'codeRow' as ToolDirective);
+                event.preventDefault();
+              }
+            } else {
+              // 接管复制快捷键
+              event.preventDefault();
+              bus.emit(editorId, 'replace', 'ctrlC');
+              break;
+            }
+            break;
           }
-          break;
-        }
-        case 'KeyZ': {
-          if (event.key === 'w') {
-            return;
-          }
-
-          if (event.shiftKey) {
-            // ctrl+shift+z 前进一步
-            if (initFunc('next')) {
-              bus.emit(editorId, 'ctrlShiftZ');
+          case 'KeyL': {
+            // ctrl+l触发普通链接
+            if (initFunc('link')) {
+              bus.emit(editorId, 'openModals', 'link');
               event.preventDefault();
             }
-          } else {
-            // ctrl+z 后退一步
-            if (initFunc('revoke')) {
-              bus.emit(editorId, 'ctrlZ');
-              event.preventDefault();
-            }
+            break;
           }
+          case 'KeyZ': {
+            if (event.key === 'w') {
+              return;
+            }
 
-          break;
-        }
-        case 'KeyW': {
-          if (event.key === 'z') {
             if (event.shiftKey) {
               // ctrl+shift+z 前进一步
               if (initFunc('next')) {
@@ -280,66 +272,88 @@ export const useKeyBoard = (props: EditorProp, staticProps: StaticProp) => {
             }
 
             break;
-          } else {
-            return;
           }
-        }
-        case 'KeyF': {
-          // ctrl+shift+f 美化内容
-          if (event.shiftKey) {
-            if (initFunc('prettier') && !noPrettier) {
-              bus.emit(editorId, 'replace', 'prettier');
-              event.preventDefault();
+          case 'KeyW': {
+            if (event.key === 'z') {
+              if (event.shiftKey) {
+                // ctrl+shift+z 前进一步
+                if (initFunc('next')) {
+                  bus.emit(editorId, 'ctrlShiftZ');
+                  event.preventDefault();
+                }
+              } else {
+                // ctrl+z 后退一步
+                if (initFunc('revoke')) {
+                  bus.emit(editorId, 'ctrlZ');
+                  event.preventDefault();
+                }
+              }
+
+              break;
+            } else {
+              return;
             }
           }
-          break;
-        }
-        case 'KeyT': {
-          // ctrl+shift+alt+t 新增表格
-          if (event.altKey && event.shiftKey) {
-            if (initFunc('table')) {
-              bus.emit(editorId, 'replace', 'table');
-              event.preventDefault();
+          case 'KeyF': {
+            // ctrl+shift+f 美化内容
+            if (event.shiftKey) {
+              if (initFunc('prettier') && !noPrettier) {
+                bus.emit(editorId, 'replace', 'prettier');
+                event.preventDefault();
+              }
             }
+            break;
           }
-          break;
+          case 'KeyT': {
+            // ctrl+shift+alt+t 新增表格
+            if (event.altKey && event.shiftKey) {
+              if (initFunc('table')) {
+                bus.emit(editorId, 'replace', 'table');
+                event.preventDefault();
+              }
+            }
+            break;
+          }
+          case 'KeyX': {
+            bus.emit(editorId, 'replace', 'ctrlX');
+            event.preventDefault();
+            break;
+          }
+          case 'KeyD': {
+            event.preventDefault();
+            bus.emit(editorId, 'replace', 'ctrlD');
+            break;
+          }
         }
-        case 'KeyX': {
-          bus.emit(editorId, 'replace', 'ctrlX');
-          event.preventDefault();
-          break;
-        }
-        case 'KeyD': {
-          event.preventDefault();
-          bus.emit(editorId, 'replace', 'ctrlD');
-          break;
+      } else if (event.code === 'Tab') {
+        event.preventDefault();
+        // 缩进
+        if (event.shiftKey) {
+          bus.emit(editorId, 'replace', 'shiftTab');
+        } else {
+          bus.emit(editorId, 'replace', 'tab');
         }
       }
-    } else if (event.code === 'Tab') {
-      event.preventDefault();
-      // 缩进
-      if (event.shiftKey) {
-        bus.emit(editorId, 'replace', 'shiftTab');
-      } else {
-        bus.emit(editorId, 'replace', 'tab');
-      }
-    }
-  };
+    },
+    [modelValue, initFunc]
+  );
 
   useEffect(() => {
+    const buildFinishedCb = (html: string) => {
+      setState(() => {
+        return {
+          buildFinished: true,
+          html
+        };
+      });
+    };
+
     if (!previewOnly) {
       window.addEventListener('keydown', keyDownHandler);
 
       bus.on(editorId, {
         name: 'buildFinished',
-        callback(html: string) {
-          setState(() => {
-            return {
-              buildFinished: true,
-              html
-            };
-          });
-        }
+        callback: buildFinishedCb
       });
     }
 
@@ -347,9 +361,10 @@ export const useKeyBoard = (props: EditorProp, staticProps: StaticProp) => {
     return () => {
       if (!previewOnly) {
         window.removeEventListener('keydown', keyDownHandler);
+        bus.remove(editorId, 'buildFinished', buildFinishedCb);
       }
     };
-  }, []);
+  }, [keyDownHandler]);
 
   useEffect(() => {
     // 每次更新都重新注册save监听
@@ -390,7 +405,7 @@ export const useKeyBoard = (props: EditorProp, staticProps: StaticProp) => {
     return () => {
       bus.remove(editorId, ON_SAVE, callback);
     };
-  }, [props.modelValue, state.buildFinished, state.html]);
+  }, [modelValue, state.buildFinished, state.html]);
 
   useEffect(() => {
     // 编辑后添加未编译完成标识
@@ -400,7 +415,7 @@ export const useKeyBoard = (props: EditorProp, staticProps: StaticProp) => {
         buildFinished: false
       };
     });
-  }, [props.modelValue]);
+  }, [modelValue]);
 };
 
 /**
