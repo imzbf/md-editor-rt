2deea131b706d17c44935654657007715aa9b3a0
zbf <zbfcqtl@gmail.com>
2023-02-09 20:44:13 +0800

    chore: open browser window on startup

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
     publicDir: mode === 'production' ? false : './public',
     server: {
       host: 'localhost',
-      open: true,
+      open: '/md-editor-rt',
       port: 2234,
       https: false
     },
