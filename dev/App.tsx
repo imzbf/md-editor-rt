005a8cbe3e7e9bee2959513fc90ded4d16373baa
zbf <zbfcqtl@gmail.com>
2023-03-02 12:50:10 +0800

    chore: store debug parameters

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Preview from './Preview';
 import PreviewOnly from './PreviewOnly';
@@ -7,10 +7,25 @@ import './style.less';
 export type Theme = 'dark' | 'light';
 
 function App() {
-  const [theme, setTheme] = useState<Theme>('light');
-  const [previewTheme, setPreviewTheme] = useState<string>('default');
-  const [codeTheme, setCodeTheme] = useState<string>('atom');
-  const [lang, setLang] = useState<'zh-CN' | 'en-US'>('zh-CN');
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem('theme') as Theme) || 'light'
+  );
+  const [previewTheme, setPreviewTheme] = useState<string>(
+    () => localStorage.getItem('previewTheme') || 'default'
+  );
+  const [codeTheme, setCodeTheme] = useState<string>(
+    () => localStorage.getItem('codeTheme') || 'atom'
+  );
+  const [lang, setLang] = useState<'zh-CN' | 'en-US'>(
+    () => (localStorage.getItem('lang') as 'zh-CN' | 'en-US') || 'zh-CN'
+  );
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    localStorage.setItem('previewTheme', previewTheme);
+    localStorage.setItem('codeTheme', codeTheme);
+    localStorage.setItem('lang', lang);
+  }, [codeTheme, lang, previewTheme, theme]);
 
   return (
     <div className={['app', theme === 'dark' && 'theme-dark'].join(' ')}>
