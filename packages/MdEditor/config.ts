32bcb2b20a24793e946021c5359b90d3af51fbef
Rizki Aprita <vallerydelexy@gmail.com>
2023-07-24 11:10:26 +0700

    added language support: Bahasa Indonesia and Basa Jawa.
    language code 
    1. bahasa indonesia: 'id-ID'
    2. basa jawa: 'jv-ID'

--- packages/MdEditor/config.ts
+++ packages/MdEditor/config.ts
@@ -233,6 +233,167 @@ export const staticTextDefault: StaticTextDefault = {
       markdownTotal: 'Word Count',
       scrollAuto: 'Scroll Auto'
     }
+  },
+  'id-ID': {
+    toolbarTips: {
+      bold: 'tebal',
+      underline: 'garis bawah',
+      italic: 'miring',
+      strikeThrough: 'coret sambung',
+      title: 'judul',
+      sub: 'subscript',
+      sup: 'superscript',
+      quote: 'quote',
+      unorderedList: 'daftar tak berurutan',
+      orderedList: 'daftar berurutan',
+      task: 'daftar tugas',
+      codeRow: 'kode inline',
+      code: 'kode blok',
+      link: 'tautan',
+      image: 'gambar',
+      table: 'tabel',
+      mermaid: 'mermaid',
+      katex: 'rumus',
+      revoke: 'membatalkan',
+      next: 'membatalkan pembatalan',
+      save: 'simpan',
+      prettier: 'penataan',
+      pageFullscreen: 'layar penuh di halaman',
+      fullscreen: 'layar penuh',
+      preview: 'pratinjau',
+      htmlPreview: 'pratinjau html',
+      catalog: 'daftar isi',
+      github: 'kode sumber'
+    },
+    titleItem: {
+      h1: 'Judul 1',
+      h2: 'Judul 2',
+      h3: 'Judul 3',
+      h4: 'Judul 4',
+      h5: 'Judul 5',
+      h6: 'Judul 6'
+    },
+    imgTitleItem: {
+      link: 'Tambahkan Tautan Gambar',
+      upload: 'Unggah Gambar',
+      clip2upload: 'Potong dan Unggah'
+    },
+    linkModalTips: {
+      linkTitle: 'Tambahkan Tautan',
+      imageTitle: 'Tambahkan Gambar',
+      descLabel: 'Deskripsi:',
+      descLabelPlaceHolder: 'Masukkan deskripsi...',
+      urlLabel: 'Tautan:',
+      urlLabelPlaceHolder: 'Masukkan tautan...',
+      buttonOK: 'OK'
+    },
+    clipModalTips: {
+      title: 'Potong dan Unggah Gambar',
+      buttonUpload: 'Unggah'
+    },
+    copyCode: {
+      text: 'Salin',
+      successTips: 'Tersalin!',
+      failTips: 'Gagal menyalin!'
+    },
+    mermaid: {
+      flow: 'diagram aliran',
+      sequence: 'diagram urutan',
+      gantt: 'diagram gantt',
+      class: 'diagram kelas',
+      state: 'diagram status',
+      pie: 'diagram pie',
+      relationship: 'diagram hubungan',
+      journey: 'diagram perjalanan'
+    },
+    katex: {
+      inline: 'rumus inline',
+      block: 'rumus blok'
+    },
+    footer: {
+      markdownTotal: 'Jumlah Kata',
+      scrollAuto: 'Gulir Otomatis'
+    },
+    'jv-ID': {
+    toolbarTips: {
+      bold: 'tebal',
+      underline: 'garis ing ngisor',
+      italic: 'miring',
+      strikeThrough: 'goyang ngisor',
+      title: 'judul',
+      sub: 'subscript',
+      sup: 'superscript',
+      quote: 'quote',
+      unorderedList: 'daftar ora urut',
+      orderedList: 'daftar urut',
+      task: 'daftar tugas',
+      codeRow: 'kode inline',
+      code: 'kode blok',
+      link: 'tautan',
+      image: 'gambar',
+      table: 'tabel',
+      mermaid: 'mermaid',
+      katex: 'rumus',
+      revoke: 'mungkasi',
+      next: 'mlaku mungkasi',
+      save: 'simpen',
+      prettier: 'rapi',
+      pageFullscreen: 'layar petengan ing kaca',
+      fullscreen: 'layar petengan',
+      preview: 'pratonton',
+      htmlPreview: 'pratonton html',
+      catalog: 'daftar isine',
+      github: 'kode sumber'
+    },
+    titleItem: {
+      h1: 'Judhul 1',
+      h2: 'Judhul 2',
+      h3: 'Judhul 3',
+      h4: 'Judhul 4',
+      h5: 'Judhul 5',
+      h6: 'Judhul 6'
+    },
+    imgTitleItem: {
+      link: 'Tambahi Tautan Gambar',
+      upload: 'Unggah Gambar',
+      clip2upload: 'Potong lan Unggah'
+    },
+    linkModalTips: {
+      linkTitle: 'Tambahi Tautan',
+      imageTitle: 'Tambahi Gambar',
+      descLabel: 'Deskripsi:',
+      descLabelPlaceHolder: 'Tulis deskripsi...',
+      urlLabel: 'Tautan:',
+      urlLabelPlaceHolder: 'Tulis tautan...',
+      buttonOK: 'OK'
+    },
+    clipModalTips: {
+      title: 'Potong lan Unggah Gambar',
+      buttonUpload: 'Unggah'
+    },
+    copyCode: {
+      text: 'Salin',
+      successTips: 'Tersalin!',
+      failTips: 'Gagal nyalin!'
+    },
+    mermaid: {
+      flow: 'diagram aliran',
+      sequence: 'diagram urutan',
+      gantt: 'diagram gantt',
+      class: 'diagram kelas',
+      state: 'diagram status',
+      pie: 'diagram pie',
+      relationship: 'diagram hubungan',
+      journey: 'diagram perjalanan'
+    },
+    katex: {
+      inline: 'rumus inline',
+      block: 'rumus blok'
+    },
+    footer: {
+      markdownTotal: 'Jumlah Téks',
+      scrollAuto: 'Gulung Otomatis'
+    }
   }
 };
 
