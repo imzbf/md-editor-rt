1863b464ef804f0432607f6d603a1e9658f3f833
imzbf <zbfcqtl@gmail.com>
2023-11-12 17:12:16 +0800

    fix: support user defined highlight option

--- packages/MdEditor/layouts/Content/hooks/useMarkdownIt.ts
+++ packages/MdEditor/layouts/Content/hooks/useMarkdownIt.ts
@@ -128,8 +128,17 @@ const useMarkdownIt = (props: ContentPreviewProps, previewOnly: boolean) => {
       md_.use(item.plugin, item.options);
     });
 
+    const userDefHighlight = md_.options.highlight;
+
     md_.set({
-      highlight: (str, language) => {
+      highlight: (str, language, attrs) => {
+        if (userDefHighlight) {
+          const result = userDefHighlight(str, language, attrs);
+          if (result) {
+            return result;
+          }
+        }
+
         let codeHtml;
 
         // 不高亮或者没有实例，返回默认
