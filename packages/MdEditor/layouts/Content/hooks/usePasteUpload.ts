d1366f34579946adb2f8bdb730b553efcae76e33
zbf <zbfcqtl@gmail.com>
2024-04-22 15:15:39 +0800

    refactor: supports converting all images within pasted content

--- packages/MdEditor/layouts/Content/hooks/usePasteUpload.ts
+++ packages/MdEditor/layouts/Content/hooks/usePasteUpload.ts
@@ -74,7 +74,7 @@ const usePasteUpload = (
       // 图片语法在当前行开头
       const templateStart = /!\[.*\]\(\s*$/.test(lineStart);
       // 图片语法在粘贴的内容中
-      const templateIn = /^!\[.*\]\((.*)\s?.*\)$/.test(targetValue);
+      const templateIn = /!\[.*\]\((.*)\s?.*\)/.test(targetValue);
 
       if (templateStart) {
         const tv = props.transformImgUrl(targetValue);
@@ -83,10 +83,25 @@ const usePasteUpload = (
         e.preventDefault();
         return;
       } else if (templateIn) {
-        const matchArr = targetValue.match(/(?<=!\[.*\]\()\S+(?=\s?["']?.*["']?\))/);
-        const tv = matchArr ? props.transformImgUrl(matchArr[0]) : targetValue;
+        const matchArr = targetValue.match(
+          /(?<=!\[.*\]\()([^)\s]+)(?=\s?["']?.*["']?\))/g
+        );
 
-        imgInsert(tv);
+        if (matchArr) {
+          Promise.all(
+            matchArr.map((img: string) => {
+              return props.transformImgUrl(img);
+            })
+          ).then((newUrls: string[]) => {
+            imgInsert(
+              newUrls.reduce((prev, curr, index) => {
+                return prev.replace(matchArr[index], curr);
+              }, targetValue)
+            );
+          });
+        } else {
+          imgInsert(targetValue);
+        }
 
         e.preventDefault();
         return;
