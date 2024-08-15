d3b56fd8394a7f6d058aee91fd9b7046b4750ae2
zbf <zbfcqtl@gmail.com>
2024-08-15 10:10:57 +0800

    refactor: upgrade util

--- packages/MdEditor/layouts/Content/hooks/useMermaid.ts
+++ packages/MdEditor/layouts/Content/hooks/useMermaid.ts
@@ -1,5 +1,5 @@
 import { useCallback, useContext, useEffect, useRef, useState } from 'react';
-import { uuid } from '@vavt/util';
+import { randomId } from '@vavt/util';
 import { prefix, configOption } from '~/config';
 import { EditorContext } from '~/Editor';
 import { appendHandler } from '~/utils/dom';
@@ -103,7 +103,7 @@ const useMermaid = (props: ContentPreviewProps) => {
         let mermaidHtml = mermaidCache.get(item.innerText) as string;
 
         if (!mermaidHtml) {
-          const idRand = uuid();
+          const idRand = randomId();
           // @9以下使用renderAsync，@10以上使用render
           const render = mermaidRef.current.renderAsync || mermaidRef.current.render;
 
