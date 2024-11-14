7ef9d71d45bcfc72fcd426101d91718d7567f02f
zbf <zbfcqtl@gmail.com>
2024-11-14 09:23:03 +0800

    fix: 统计资源无法加载

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,21 +14,24 @@ export default function Document() {
         <NextScript />
         <Script src="//at.alicdn.com/t/c/font_2818624_gbt6qvt9lob.js" />
         {process.env.NODE_ENV === 'production' && (
-          <>
-            <Script
-              async
-              src="https://www.googletagmanager.com/gtag/js?id=G-3R6P1XE0H0"
-            />
-            <Script id="gg">
-              {`window.dataLayer = window.dataLayer || [];
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-3R6P1XE0H0"
+          />
+        )}
+        {process.env.NODE_ENV === 'production' && (
+          <Script id="gg">
+            {`window.dataLayer = window.dataLayer || [];
                 function gtag() {
                   dataLayer.push(arguments);
                 }
                 gtag('js', new Date());
                 gtag('config', 'G-3R6P1XE0H0');`}
-            </Script>
-            <Script src="https://hm.baidu.com/hm.js?1563bc52cb27ffbc7b5b46cdfc327ce0" />
-          </>
+          </Script>
+        )}
+
+        {process.env.NODE_ENV === 'production' && (
+          <Script src="https://hm.baidu.com/hm.js?1563bc52cb27ffbc7b5b46cdfc327ce0" />
         )}
       </body>
     </Html>
