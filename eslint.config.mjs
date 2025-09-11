0b2d7de030f2516521483410adcf499d690cd94c
zbf <zbfcqtl@gmail.com>
2025-09-11 09:46:48 +0800

    chore: replace `tseslint.config` with `defineConfig`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,5 @@
-// import js from '@eslint/js';
-// import globals from 'globals';
-// import reactHooks from 'eslint-plugin-react-hooks';
-// import reactRefresh from 'eslint-plugin-react-refresh';
-// import tseslint from 'typescript-eslint';
-
-// export default tseslint.config(
-//   { ignores: ['lib'] },
-//   {
-//     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-//     files: ['**/*.{ts,tsx}'],
-//     languageOptions: {
-//       ecmaVersion: 2020,
-//       globals: globals.browser
-//     },
-//     plugins: {
-//       'react-hooks': reactHooks,
-//       'react-refresh': reactRefresh
-//     },
-//     rules: {
-//       ...reactHooks.configs.recommended.rules,
-//       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-//       '@typescript-eslint/no-explicit-any': 'off'
-//     }
-//   }
-// );
-
 import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
@@ -34,7 +8,7 @@ import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['lib', 'eslint.config.mjs']
   },
